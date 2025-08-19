779c66e2c69b9a91d7dec9e08d7c42ba98331b46
Paul Floyd <pjfloyd@wanadoo.fr>
2025-08-19 16:37:50 +0200

    Bug 507721 - Wire up illumos and Solaris mallinfo

--- coregrind/m_replacemalloc/vg_replace_malloc.c
+++ coregrind/m_replacemalloc/vg_replace_malloc.c
@@ -2591,8 +2591,9 @@ static void panic(const char *str)
  MALLINFO(VG_Z_LIBC_SONAME, mallinfo);
  MALLINFO(SO_SYN_MALLOC,    mallinfo);
 
-#elif defined(VGO_darwin)
- //MALLINFO(VG_Z_LIBC_SONAME, mallinfo);
+#elif defined(VGO_solaris)
+ MALLINFO(VG_Z_LIBC_SONAME, mallinfo);
+ MALLINFO(SO_SYN_MALLOC,    mallinfo);
 
 #endif
 
@@ -2617,9 +2618,6 @@ static void panic(const char *str)
 #if defined(VGO_linux)
  MALLINFO2(VG_Z_LIBC_SONAME, mallinfo2);
  MALLINFO2(SO_SYN_MALLOC,    mallinfo2);
-
-#elif defined(VGO_darwin)
- //MALLINFO2(VG_Z_LIBC_SONAME, mallinfo2);
 
 #endif
 
