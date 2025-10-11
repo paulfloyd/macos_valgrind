5b64c91094a2f145607e523fe52ffafa64dcfd0b
Paul Floyd <pjfloyd@wanadoo.fr>
2025-10-11 22:38:02 +0200

    Use VG_(try_get_interp)

--- coregrind/m_initimg/initimg-darwin.c
+++ coregrind/m_initimg/initimg-darwin.c
@@ -493,7 +493,7 @@ Addr setup_client_stack( void*  init_sp,
    if (VG_(resolved_exename) == NULL) {
       const HChar *exe_name = VG_(find_executable)(VG_(args_the_exename));
       HChar interp_name[VKI_PATH_MAX];
-      if (try_get_interp(exe_name, interp_name)) {
+      if (VG_(try_get_interp)(exe_name, interp_name, VKI_PATH_MAX)) {
          exe_name = interp_name;
       }
       HChar resolved_name[VKI_PATH_MAX];
